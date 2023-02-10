7b0b373d422791ec85e0bf91c32d343c69bcc3ab
RubenPerez <ruben.perez@civica-soft.com>
2023-02-10 13:51:48 +0100

    fix_agr_mart

--- dbt_brooklyn/models/mart/tpch_sf1/agg_tpch_sf1__item_year_shipmode.sql
+++ dbt_brooklyn/models/mart/tpch_sf1/agg_tpch_sf1__item_year_shipmode.sql
@@ -4,46 +4,53 @@ WITH stg_tpch_sf1_customers AS (
         C_NATIONKEY
     FROM {{ ref ('stg_tpch_sf1__customer') }}
 ),
-WITH stg_tpch_sf1_lineitem AS (
+stg_tpch_sf1_lineitem AS (
     SELECT 
         L_ORDERKEY,
         L_PARTKEY,
+        L_SHIPMODE,
         L_SHIPDATE
     FROM {{ ref ('stg_tpch_sf1__lineitem') }}
 ),
-WITH stg_tpch_sf1_nation AS (
+stg_tpch_sf1_nation AS (
     SELECT 
         N_NATIONKEY,
+        N_REGIONKEY,
         N_NAME
     FROM {{ ref ('stg_tpch_sf1__nation') }}
 ),
-WITH stg_tpch_sf1_region AS (
+stg_tpch_sf1_region AS (
     SELECT 
         R_REGIONKEY,
         R_NAME
     FROM {{ ref ('stg_tpch_sf1__region') }}
 ),
-WITH stg_tpch_sf1_orders AS (
+stg_tpch_sf1_orders AS (
     SELECT 
         O_ORDERKEY,
         O_CUSTKEY,
         O_ORDERDATE
     FROM {{ ref ('stg_tpch_sf1__orders') }}
 ),
-agr_region_nation as (
-    SELECT 
-        REG.R_NAME AS REGION,
-        NAT.N_NAME AS NATION,
-        COUNT(DISTINCT CUS.C_CUSTKEY) AS NUM_CUSTOMERS
+agr_items_year_shipmode as (
+    SELECT
+        year(LIN.L_SHIPDATE) AS YEAR,
+        LIN.L_SHIPMODE,
+        COUNT(DISTINCT LIN.L_PARTKEY) AS NUM_ITEM
     FROM
         stg_tpch_sf1_customers CUS
         INNER JOIN stg_tpch_sf1_nation NAT ON CUS.C_NATIONKEY = NAT.N_NATIONKEY
         INNER JOIN stg_tpch_sf1_region REG ON NAT.N_REGIONKEY = REG.R_REGIONKEY
         INNER JOIN stg_tpch_sf1_orders ORD ON CUS.C_CUSTKEY = ORD.O_CUSTKEY
         INNER JOIN stg_tpch_sf1_lineitem LIN ON LIN.L_ORDERKEY = ORD.O_ORDERKEY
-    GROUP BY 
-        REG.R_NAME,
-        NAT.N_NAME;
-)
+    GROUP BY
+        year(LIN.L_SHIPDATE),
+        LIN.L_SHIPMODE
+)  
 
-SELECT * FROM agr_region_nation
+SELECT * FROM agr_items_year_shipmode
+
+
+
+
+  
--- dbt_brooklyn/models/mart/tpch_sf1/agg_tpch_sf1__orders_year.sql
+++ dbt_brooklyn/models/mart/tpch_sf1/agg_tpch_sf1__orders_year.sql
@@ -4,37 +4,38 @@ WITH stg_tpch_sf1_customers AS (
         C_NATIONKEY
     FROM {{ ref ('stg_tpch_sf1__customer') }}
 ),
-WITH stg_tpch_sf1_lineitem AS (
+stg_tpch_sf1_lineitem AS (
     SELECT 
         L_ORDERKEY,
         L_PARTKEY,
+        L_SHIPMODE,
         L_SHIPDATE
     FROM {{ ref ('stg_tpch_sf1__lineitem') }}
 ),
-WITH stg_tpch_sf1_nation AS (
+stg_tpch_sf1_nation AS (
     SELECT 
         N_NATIONKEY,
+        N_REGIONKEY,
         N_NAME
     FROM {{ ref ('stg_tpch_sf1__nation') }}
 ),
-WITH stg_tpch_sf1_region AS (
+stg_tpch_sf1_region AS (
     SELECT 
         R_REGIONKEY,
         R_NAME
     FROM {{ ref ('stg_tpch_sf1__region') }}
 ),
-WITH stg_tpch_sf1_orders AS (
+stg_tpch_sf1_orders AS (
     SELECT 
         O_ORDERKEY,
         O_CUSTKEY,
         O_ORDERDATE
     FROM {{ ref ('stg_tpch_sf1__orders') }}
 ),
-agr_items_year_shipmode as (
+agr_orders_year as (
     SELECT
-        year(LIN.L_SHIPDATE) AS YEAR,
-        LIN.L_SHIPMODE,
-        COUNT(DISTINCT LIN.L_PARTKEY) AS NUM_ITEM
+        year(ORD.O_ORDERDATE) AS YEAR,
+        COUNT(DISTINCT ORD.O_ORDERDATE) AS NUM_ORDERS
     FROM
         stg_tpch_sf1_customers CUS
         INNER JOIN stg_tpch_sf1_nation NAT ON CUS.C_NATIONKEY = NAT.N_NATIONKEY
@@ -42,13 +43,7 @@ agr_items_year_shipmode as (
         INNER JOIN stg_tpch_sf1_orders ORD ON CUS.C_CUSTKEY = ORD.O_CUSTKEY
         INNER JOIN stg_tpch_sf1_lineitem LIN ON LIN.L_ORDERKEY = ORD.O_ORDERKEY
     GROUP BY
-        year(LIN.L_SHIPDATE),
-        LIN.L_SHIPMODE;
-)  
+        year(ORD.O_ORDERDATE)
+)
 
-SELECT * FROM agr_items_year_shipmode
-
-
-
-
-  
+SELECT * FROM agr_orders_year
